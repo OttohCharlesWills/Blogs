cf0ad937c392787355457f9854f7836ceaacd6e9
ottohcharleswills <ottohcharleswills@gmail.com>
2024-12-22 17:23:25 +0100

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('wrapper');
-    var scrollThreshold = 200; // Change color after scrolling 30px
-
-    if (window.scrollY >= scrollThreshold) {
-      navbar.classList.add('try');
-    } else {
-      navbar.classList.remove('try');
-    }
-  });
-
-
-
-
-
   // FOR SIDENAV
   let openBtn = document.getElementById("openBtn");
   let closeBtn = document.getElementById("closeBtn");
@@ -28,4 +13,19 @@ window.addEventListener('scroll', function() {
       sideNav.style.display = "none";
       body.style.overflow = ""; // Re-enable scrolling
   });
+
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('wrapper');
+    var scrollThreshold = 200; // Change color after scrolling 30px
+
+    if (window.scrollY >= scrollThreshold) {
+      navbar.classList.add('try');
+    } else {
+      navbar.classList.remove('try');
+    }
+  });
+
+
+
   
