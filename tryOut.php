4d9cee980572fa479a6420d50f0b046702958d09
ottohcharleswills <ottohcharleswills@gmail.com>
2025-01-05 20:49:46 +0100

    Third Changes

---
+++ tryOut.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // CODE TO CHECK FOR LEAP YEARS
+    // $year = 2000;
+    // $dividant = 4;
+
+    // function Modulus($year, $dividant){
+    //     $answer = $year % $dividant;
+    //     return $answer;
+    // }
+
+    // echo "The Number Is : ". Modulus($year, $dividant);
+    ?>
+</body>
+</html>
